e34ca9d6182fbece2aaa22e8c34ac214bdb45b67
lay-d <lays.diachuk.pereira@escola.pr.gov.br>
2026-03-05 12:01:35 -0300

    Add files via upload

--- projeto_hotel/projetohotel.lays2B/backend/app.py
+++ projeto_hotel/projetohotel.lays2B/backend/app.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask, send_from_directory
 
 
 # caminho base do projeto (uma pasta acima do backend)
-BASE_DIR = os.path.abspat(os.path.join(os.path.dirme(__file__), ".."))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 # pasta frontend (html e js)
 FRONTEND_DIR = os.path.join(BASE_DIR, "frontend")
@@ -13,10 +14,27 @@ STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 app = Flask(__name__, static_folder=STATIC_DIR,static_url_path="/"+ STATIC_DIR)
 
+# =================================
+
+# ROTA PRINCIPAL (HTML)
+#ROTAS DA PAGINA
+#Pagina inicial
+# ================================
 
 @app.route("/")
 def home():
-    return "Bom dia galera 2B!"
+    return send_from_directory(FRONTEND_DIR,"index.html")
 
+
+#
+@app.route("/consulta")
+def consultar_page():
+    return send_from_directory(FRONTEND_DIR, "consulta.html")
+
+
+#
+@app.route("/alterar")
+def alterar_page():
+    return send_from_directory(FRONTEND_DIR, "alterar.html")
 
 app.run()
